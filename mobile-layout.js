0d49e1e5d792f32f5faf6651ee3a474a0fe79552
secnyt <47509759+secnyt@users.noreply.github.com>
2020-10-28 10:04:17 -0500

    make sidebar responsive for mobile users

---
+++ mobile-layout.js
@@ -0,0 +1,28 @@
+
+var content = document.getElementsByClassName('content')[0];
+var sidebar = document.getElementsByClassName('sidebar')[0];
+var footer = document.getElementsByClassName('footer')[0];
+
+var checkMobile = () => {
+  if ( window.innerWidth < 800 ) {
+    content.classList.remove("content");
+    content.classList.add("mobile-content");
+
+    sidebar.classList.remove("sidebar");
+    sidebar.classList.add("mobile-sidebar");
+
+    footer.classList.remove('footer');
+    footer.classList.add('mobile-footer');
+  } else {
+    content.classList.add("content");
+    content.classList.remove("mobile-content");
+
+    sidebar.classList.add("sidebar");
+    sidebar.classList.remove("mobile-sidebar");
+
+    footer.classList.add('footer');
+    footer.classList.remove('mobile-footer');
+  }
+}
+checkMobile();
+window.addEventListener('resize', checkMobile);
