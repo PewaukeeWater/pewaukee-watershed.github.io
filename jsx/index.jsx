4c4d02064ac11875de23e4375d9493540a7c3aa4
KhajasJS <57632215+KhajasJS@users.noreply.github.com>
2020-12-11 10:21:47 -0600

    Use object for state

--- jsx/index.jsx
+++ jsx/index.jsx
@@ -1,14 +1,14 @@
 class App extends React.Component {
   constructor(){
     super()
-    this.state = false
+    this.state = { clicked: false }
   }
   render(){
-    if(this.state) {
+    if(this.state.clicked) {
       return <h1>I was clicked</h1>
     } else {
       const onClick = () => {
-        this.setState(true)
+        this.setState({ clicked: true })
       }
       return <button onClick={onClick}>Button</button>
     }
