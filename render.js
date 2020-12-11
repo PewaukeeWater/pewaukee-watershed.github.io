03ffb586cccf8b67861f7b7f1c6582ec780589fa
Compiler Actions <compiler-actions[bot]>
2020-12-11 16:16:37 +0000

    Compile JSX to JS and HTML

---
+++ render.js
@@ -0,0 +1,11 @@
+
+import(`./${import.meta.url
+  .split('?')[1]
+  .split('&')
+  .map(p => p.split('='))
+  .find(([p]) => p === 'component')[1]}`
+).then(({ default: App }) => {
+  const app = React.createElement(App)
+  const appDiv = document.getElementById('app')
+  ReactDOM.hydrate(app, appDiv)
+})
